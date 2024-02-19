ea9f48a6c0c8e4764df74cd1dcf2e21f15a320a0
Narendra <narendra2kee@gmail.com>
2024-02-19 22:54:11 +0530

    added try catch block to display syntax error

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
+
+
 function appendToDisplay(input) {
     display.value += input;
   }
   function clearScreen() {
     display.value = "";
   }
-  function calculate() {
-    display.value = eval(display.value)
+ 
+    function calculate() {
+        try{
+        display.value = eval(display.value)
+      }catch(error){
+        display.value ="error"
+      }
   }
+  
